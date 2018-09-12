24aadb3464c59f81b9b5318154af305db4f6815b
G4dus <32588510+G4dus@users.noreply.github.com>
2018-09-12 22:34:54 +0200

    Update GetMachineName.cs

--- GetMachineName.cs
+++ GetMachineName.cs
@@ -1,10 +1,29 @@
 using System;
+using System.IO;
 
-class Sample 
-{
-    public static void Main() 
+ public static void Main()
     {
-    Console.WriteLine();
-    Console.WriteLine("MachineName: {0}", Environment.MachineName);
+        DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+        foreach (DriveInfo d in allDrives)
+        {
+            Console.WriteLine("Drive {0}", d.Name);
+            Console.WriteLine("  Drive type: {0}", d.DriveType);
+            if (d.IsReady == true)
+            {
+                Console.WriteLine("  Volume label: {0}", d.VolumeLabel);
+                Console.WriteLine("  File system: {0}", d.DriveFormat);
+                Console.WriteLine(
+                    "  Available space to current user:{0, 15} bytes", 
+                    d.AvailableFreeSpace);
+
+                Console.WriteLine(
+                    "  Total available space:          {0, 15} bytes",
+                    d.TotalFreeSpace);
+
+                Console.WriteLine(
+                    "  Total size of drive:            {0, 15} bytes ",
+                    d.TotalSize);
+            }
+        }
     }
-}
